304fb55d9dd69ec95738758843f486998b9a9ea9
Amir.liquinder <amir505553amir@gmail.com>
2020-01-27 15:16:06 +0300

    Release v.1.0

---
+++ Assets/ARFiles/Scripts/PlayingAudio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayingAudio : MonoBehaviour
+{
+    public AudioClip _audioClip;
+    private AudioSource _audioSource;
+
+    private ChangedTrackableEventHandler _trackableEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = _audioClip;
+
+        _trackableEvent = GetComponentInParent<ChangedTrackableEventHandler>();
+        _trackableEvent.NotifyOnTrackingFound += TrackableEvent_NotifyOnTrackingFound;
+        _trackableEvent.NotifyOnTrackingLost += TrackableEvent_NotifyOnTrackingLost;
+    }
+
+    private void TrackableEvent_NotifyOnTrackingLost()
+    {
+        _audioSource.Stop();
+    }
+
+    private void TrackableEvent_NotifyOnTrackingFound()
+    {
+        _audioSource.Play();
+    }
+}
